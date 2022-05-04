fdc2ab0bb9179a3ceab35af43541649f54c5ee0f
PepijndeReus <pepijn.dereus@protonmail.com>
2022-05-04 15:34:26 +0200

    python file for measurement adult kNN

---
+++ Benchmark/K_nearest.py
@@ -0,0 +1,44 @@
+"""
+This file uses pyRAPL to measure the energyconsumption of the k-nearest neighbours machine learning model on both the adult and student data set.
+"""
+
+# import modules
+import pandas as pd
+import numpy as np
+import os
+import pyRAPL
+# import time
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+
+# change directory for Adult data set
+os.chdir('../Data/Adult')
+
+# measure energy consumption
+pyRAPL.setup()
+meter = pyRAPL.Measurement('kNN')
+meter.begin()
+
+# load data
+adult_train_data = pd.read_csv('Adult_train_data.csv')
+adult_train_labels = pd.read_csv('Adult_train_labels.csv')
+adult_val_data = pd.read_csv('Adult_val_data.csv')
+adult_val_labels = pd.read_csv('Adult_val_labels.csv')
+
+# list with k's
+list_k = [2,3,5,7,10]
+
+# make kNN for each k value in list
+for k in list_k:
+    # make and fit kNN model
+    NeighbourModel = KNeighborsClassifier(n_neighbors=k)
+    NeighbourModel.fit(adult_train_data, adult_train_labels.values.ravel())
+
+    # make and print report
+    report = classification_report(adult_val_labels, NeighbourModel.predict(adult_val_data), output_dict=True)
+    print(f"For k = {k}, accuracy =", end=" ")
+    print(report['accuracy'])
+
+meter.end()
+meter.export('knn_adult_energy.csv')
